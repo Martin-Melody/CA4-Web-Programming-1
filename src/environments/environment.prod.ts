c4e2a2f9925162551c17dcabb2953e65e8ad9117
Martin-Melody <s00211628@mail.itsligo.ie>
2022-03-15 16:19:22 +0000

    Service files and enviroment details added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAbPTVmfxFerhCCV5XcoD0Lodjs46AmgLw',
+    authDomain: 'car-sales-application.firebaseapp.com',
+    projectId: 'car-sales-application',
+    storageBucket: 'car-sales-application.appspot.com',
+    messagingSenderId: '276912129861',
+    appId: '1:276912129861:web:530ba9cf91168f4427602c',
+    measurementId: 'G-M1YMPC1P03',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAbPTVmfxFerhCCV5XcoD0Lodjs46AmgLw',
+    authDomain: 'car-sales-application.firebaseapp.com',
+    projectId: 'car-sales-application',
+    storageBucket: 'car-sales-application.appspot.com',
+    messagingSenderId: '276912129861',
+    appId: '1:276912129861:web:530ba9cf91168f4427602c',
+    measurementId: 'G-M1YMPC1P03',
+  },
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
